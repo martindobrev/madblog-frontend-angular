0664413162741ca28f85a7474af41179aa5e45ef
Ivan Simeonov <IvanNickSimeonov@gmail.com>
2020-01-23 08:33:48 +0200

    Highlight searched text in yellow finished

--- src/app/shared/highlight.pipe.ts
+++ src/app/shared/highlight.pipe.ts
@@ -1,10 +1,20 @@
 import {PipeTransform, Pipe} from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Pipe({name: 'highlight'})
 export class HighLightPipe implements PipeTransform {
-    transform(text: string, search: string): string {
-        console.log('inside pipe: ' + search + text);
-        return search ? text.replace(new RegExp(search, 'i'),
-        '<span style="background-color: red">' + `${search}` + '</span>') : text;
+
+    constructor(private _sanitizer: DomSanitizer) {
+    }
+
+    // transform(text: string, search: string): string {
+    //     console.log('inside pipe: ' + search + text);
+    //     return search ? text.replace(new RegExp(search, 'i'),
+    //     '<strong style="background-color: red">' + `${search}` + '</strong>') : text;
+    // }
+
+    transform(text: string, search: string): SafeHtml {
+        return this._sanitizer.bypassSecurityTrustHtml( search ? text.replace(new RegExp(search, 'i'),
+        '<strong style="background-color: yellow">' + `${search}` + '</strong>') : text);
     }
 }
